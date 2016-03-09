22a5b269c66f6157406257f75e90b96ba86b5bb9
Dietrich <dietricherwane@yahoo.fr>
2016-03-09 14:43:23 +0000

    chnages

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -17,7 +17,7 @@ class DepositsController < ApplicationController
   end
 
   def api_get_pos_sale_balance
-    redirect_to @@notification_url "/api/a1b43b7d1b/pos_balance/get/#{params[:game_token]}/#{params[:pos_id]}/#{params[:session_id]}"
+    redirect_to %Q[#{@@notification_url}/api/a1b43b7d1b/pos_balance/get/#{params[:game_token]}/#{params[:pos_id]}/#{params[:session_id]}]
 =begin
     @token = params[:game_token]
     @pos_id = params[:pos_id]
