3ee744cd96e258fb48cf1917f3e04e31767ed177
Dietrich <dietricherwane@yahoo.fr>
2016-06-29 16:52:18 +0000

    changes

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1128,7 +1128,7 @@ def api_sf_validate_credit
       if is_a_number?(transaction_amount)
         transaction_id = Digest::SHA1.hexdigest([DateTime.now.iso8601(6), rand].join)
         #set_game_operation_token(game_account_token)
-        @url = "#{Parameter.first.paymoney_wallet_url}/PAYMONEY_WALLET/rest/cash_in_operation_momo_#{wallet}/#{operation_token}/#{account_token}/#{mobile_money_token}/#{transaction_amount}/#{fee}/100/#{transaction_id}}"
+        @url = "#{Parameter.first.paymoney_wallet_url}/PAYMONEY_WALLET/rest/cash_in_operation_momo_#{wallet}/#{operation_token}/#{account_token}/#{mobile_money_token}/#{transaction_amount}/#{fee}/100/#{transaction_id}"
         BombLog.create(sent_url: @url)
         response = (RestClient.get @url rescue "")
         response = response.to_s.force_encoding('iso-8859-1').encode('utf-8')
